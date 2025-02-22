0b898977f66dacb5115148600aa70d48f6b770d9
akshajnad <98061663+akshajnad@users.noreply.github.com>
2025-02-22 02:29:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  base: '/QRScout/',  // Matches your GitHub Pages subfolder
   plugins: [react()],
   resolve: {
     alias: {
